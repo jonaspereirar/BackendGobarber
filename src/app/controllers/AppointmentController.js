fefe98341f6fb1115297ecb6b1f9f0b3ac1b9acc
jprodrigues1981 <jonasprodrigues@gmail.com>
2019-09-04 23:42:31 +0100

    feat add: metod appointment creater

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -16,17 +16,23 @@ class AppointmentController {
     const { provider_id, date } = req.body;
 
     // Check if provider_id is a provider
-    const isProvider = await User.findOne({
+    const checkIsProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
 
-    if (!isProvider) {
+    if (!checkIsProvider) {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
     }
 
-    return res.json();
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
   }
 }
 
