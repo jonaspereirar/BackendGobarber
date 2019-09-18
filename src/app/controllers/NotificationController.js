adf7ebc2f576f0f1c0b937c49a262d4ade9afcad
jprodrigues1981 <jonasprodrigues@gmail.com>
2019-09-18 23:26:14 +0100

    feat. add. check if notification reads and updates

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -21,6 +21,16 @@ class NotificationController {
 
     return res.json(notifications);
   }
+
+  // check if notification reads and updates
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+    return res.json(notification);
+  }
 }
 
 export default new NotificationController();
