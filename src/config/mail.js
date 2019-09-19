6259fd902124c46208c4153b993e15ca2b77bde7
jprodrigues1981 <jonasprodrigues@gmail.com>
2019-09-19 23:26:58 +0100

    feat add: notify by email appointment canceled

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8d3736eb5b3434',
+    pass: '4af712c67f62c1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+
+// Mailtrap (DEV)
