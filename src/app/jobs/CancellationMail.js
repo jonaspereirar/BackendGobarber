8e41c9b52345c98b821a305e30f4290f9f6c1baa
jprodrigues1981 <jonasprodrigues@gmail.com>
2019-09-21 23:37:45 +0100

    feat add: Running Bee-Queue - Failures detection

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -11,7 +11,7 @@ class CancellationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    console.log('A fila executou');
+    // console.log('A fila executou');
 
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
